fc42c7f09f413f0c7a893fcf5e96c2adc94829c2
Saifullah-Afridi <saifullahafridi08@gmail.com>
2024-06-29 21:55:08 +0500

    update

--- api/app.js
+++ api/app.js
@@ -24,6 +24,21 @@ const port = 3000;
 app.listen(process.env.port || port, () => {
   console.log("The server is listening on port " + port);
 });
+
+app.use(function (req, res, next) {
+  console.log(req.method, req.originalUrl);
+  // res.send("two");
+  return;
+  next();
+});
+
+//for maintaince
+//as we not provided the next() so the other midlleware will not runs
+//and the middleware chian will be exited after sending the request
+//return statement will send the res and exits the middleware chain
+app.use(function (req, res, next) {
+  return res.status(503).send("The Server is Down,Please try  agian later");
+});
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1/user", userRoutes);
